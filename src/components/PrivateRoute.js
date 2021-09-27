023105007f978c4f6b13ef1225a0805ca7b9be19
aksingh47 <56232421+aksingh47@users.noreply.github.com>
2021-09-27 17:22:15 +0530

    fix private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -2,8 +2,6 @@ import {React} from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
-const isLogin = () => true
-
 const PrivateRoute = ({component: Component, ...rest}) => {
     return (
 
